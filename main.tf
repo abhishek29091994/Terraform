a929d491c3d555cc29181eedfe7aa13cc8714b00
abhishek29091994 <abhisheksaibannawar29@gmail.com>
2025-03-04 19:45:26 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+region="ap-south-1"
+}
+resource "aws_instance" "one" {
+  count         = 2
+  ami           = "ami-0f2ce9ce760bd7133"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Dev-server"
+  }
+}
