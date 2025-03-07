14decec1006046e5867b5b26e903421ae9765e1d
abhishek29091994 <abhisheksaibannawar29@gmail.com>
2025-03-07 22:41:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
 region="ap-south-1"
 }
 resource "aws_instance" "one" {
-  count         = 2
+  count         = 3
   ami           = "ami-0f2ce9ce760bd7133"
   instance_type = "t2.medium"
   tags = {
